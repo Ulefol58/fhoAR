6def7c8e4f314e4bd0f8fa015fc7fe2626405ef0
Ulefol58 <154829069+Ulefol58@users.noreply.github.com>
2023-12-25 22:20:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -30,23 +30,3 @@ prevArrow.addEventListener("click", prevSlide);
 nextArrow.addEventListener("click", nextSlide);
 
 showSlide(currentSlide);
-<script>
-    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
-
-    window.onscroll = function () {
-        scrollFunction();
-    };
-
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            scrollToTopBtn.style.display = "block";
-        } else {
-            scrollToTopBtn.style.display = "none";
-        }
-    }
-
-    scrollToTopBtn.addEventListener("click", function () {
-        document.body.scrollTop = 0; // Для Safari
-        document.documentElement.scrollTop = 0; // Для других браузеров
-    });
-</script>
